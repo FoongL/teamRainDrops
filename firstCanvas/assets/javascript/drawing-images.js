879fada221b15bc6b76877e8ee2c36af795bf0ef
david zhang <dlz230@nyu.edu>
2019-12-04 09:50:52 +0800

    with image stamp

--- firstCanvas/assets/javascript/drawing-images.js
+++ firstCanvas/assets/javascript/drawing-images.js
@@ -1,24 +1,35 @@
-class StampPan extends PaintFunction {
+class ImageEggplant extends PaintFunction {
     constructor(contextReal) {
         super();
-        this.context = contextReal;
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;  
     }
     onMouseDown(coord, event) {
-        var img = new Image();
-        img.src = './img/pan.png';
+        let img = new Image();
+        img.src = './assets/images/imageicon/eggplant.png';
         img.onload =  () => {
-            this.context.drawImage(img, coord[0], coord[1], 150, 150);
+            this.contextReal.drawImage(img, coord[0], coord[1], 100, 100);
         }
     }
-    onDragging(coord, event){
-        var img = new Image();
-        img.src = './img/pan.png';
-        img.onload =  () => {  
-            this.context.drawImage(img, coord[0], coord[1], 150, 150);
-        }
-    }
-    onMouseUp(){
-        // History
-        history.push($('#canvas-real')[0].toDataURL());
-    }
+    onDragging(){}
+    onMouseUp(){}
 }
+
+class ImageClown extends PaintFunction {
+    constructor(contextReal) {
+        super();
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;  
+    }
+    onMouseDown(coord, event) {
+        let img = new Image();
+        img.src = './assets/images/imageicon/clown.png';
+        img.onload =  () => {
+            this.contextReal.drawImage(img, coord[0], coord[1], 100, 100);
+        }
+    }
+    onDragging(){}
+    onMouseUp(){}
+}
+
+
