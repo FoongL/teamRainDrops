48a017d9b7cb7d3c097f80bba7864cb19f433f0c
FoongL <leung.foong@gmail.com>
2019-12-05 17:23:44 +0800

    added descriptions to everything

--- firstCanvas/assets/javascript/init.js
+++ firstCanvas/assets/javascript/init.js
@@ -2,58 +2,59 @@
 //------------------Pen Function Buttons
 $('#drawLine').on('click', function() {
   console.log('I AM CLICKEDDDD');
-  $("#funcDesc").html(`Pencil`);
+  $("#funcDesc").html(`Pencil Drawing Tool`);
   currentFunction = new PencilLine(contextReal, lineColor);
 });
 
 $('#graffiti').on('click', function() {
-  $('#funcDesc').html('Spray Paint');
+  $('#funcDesc').html('Graffiti for your punk ass style');
     currentFunction = new Graffiti(contextReal, lineColor);
   });
   $('#marker').on('click', function() {
-    $('#funcDesc').html('Marker');
+    $('#funcDesc').html('Whiteboard Marker');
     currentFunction = new Marker(contextReal, lineColor);
   });
 //------------------Selecter
 $('#selector-button').on('click', function() {
-  $('#funcDesc').html('Selector Tool');
+  $('#funcDesc').html('Selector tool: allows you to cut out the ugly parts of your painting');
   currentFunction = new Selecting(contextReal, contextDraft);
 });
 //------------------Colors
 $('#fill').on('click', function() {
-  $('#funcDesc').html('Paint Fill');
+  $('#funcDesc').html("Paint Fill when you're too lazy to do this manually");
   currentFunction = new ColorFill(contextReal, contextDraft);
 });
 
 // David's Clicks
 $('#drawingRect').on('click', function(){
-  $('#funcDesc').html('Insert Rectangle');
+  $('#funcDesc').html('Rectangles baby!');
   currentFunction = new DrawingRectangle(contextReal,contextDraft)
 })
 
 $('#drawingTriangle').on('click', function(){
-  $('#funcDesc').html('Insert Triangle');
+  $('#funcDesc').html('Triangles baby!');
   currentFunction = new DrawingTriangle(contextReal, contextDraft)
 })
 
 $('#drawingCircle').on('click', function(){
-  $('#funcDesc').html('Insert Circle');
+  $('#funcDesc').html('Circles Baby!');
   currentFunction = new DrawingCircle(contextReal, contextDraft)
 })
 
 $('#drawingPolygon').on('click', function(){
-  $('#funcDesc').html('Draw Polygon');
+  $('#funcDesc').html('PPPOOOLLLYYYGGGOOONNN!!!');
   currentFunction = new DrawingPolygon(contextReal, contextDraft)
 });
 
 $('#drawingEraser').on('click', function(){
-  $('#funcDesc').html('Eraser');
+  $('#funcDesc').html('Erase that nasty stuff');
   currentFunction = new DrawingEraser(contextReal, contextDraft)
 })
 
 /* Image stamping */
 
 $('#drawingImages').on('click', function(){
+  $('#funcDesc').html('Pick one of our friendly emojis to add color into your dark lonely existence.');
   $('.imagebox').toggle('show');
 })
 
@@ -68,58 +69,72 @@ $('#imagebox2').click(function(){
 })
 
 $('#imagebox3').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageBanana(contextReal, contextDraft);
 })
 
 $('#imagebox4').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageIcecream(contextReal, contextDraft);
 })
 
 $('#imagebox5').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageHeart(contextReal, contextDraft);
 })
 
 $('#imagebox6').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageUnicorn(contextReal, contextDraft);
 })
 
 $('#imagebox7').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageRaccoon(contextReal, contextDraft);
 })
 
 $('#imagebox8').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageBear(contextReal, contextDraft);
 })
   
 $('#imagebox9').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageTomato(contextReal, contextDraft);
 })
   
 $('#imagebox10').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageUnderwear(contextReal, contextDraft);
 })
   
 $('#imagebox11').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageDog(contextReal, contextDraft);
 })
 
 $('#imagebox12').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageMonkey(contextReal, contextDraft);
 })
 
 $('#imagebox13').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageCat(contextReal, contextDraft);
 })
 
 $('#imagebox14').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageFlower(contextReal, contextDraft);
 })
 
 $('#imagebox15').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImagePoop(contextReal, contextDraft);
 })
 
 $('#imagebox16').click(function(){
+  $('#funcDesc').html('Clown Emoji');
   currentFunction = new ImageMoney(contextReal, contextDraft);
 })
   
@@ -130,11 +145,11 @@ $('#clearCanvas').on('click', function(){
 
 // Suruchi's Clicks
 $('#straight-line').on('click',function(){
-  $('#funcDesc').html('Straight Line');
+  $('#funcDesc').html('Straight Lines');
   currentFunction = new DrawingStraight(contextReal,contextDraft)
 })
 
 $('#curve-line').on('click',function(){
-  $('#funcDesc').html('Curved Line (Liam, I did this with NO help...)');
+  $('#funcDesc').html('Curved Lines: "Liam, I did this with NO help..." - Suruchi, 2019');
   currentFunction = new DrawingCurve(contextReal,contextDraft)
 })
