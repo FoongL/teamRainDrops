80c6063ac50a205f3a5721a0c346aa9424895b1f
FoongL <leung.foong@gmail.com>
2019-12-05 19:28:21 +0800

    added detail comments

--- firstCanvas/assets/javascript/init.js
+++ firstCanvas/assets/javascript/init.js
@@ -3,51 +3,61 @@
 $('#drawLine').on('click', function() {
   console.log('I AM CLICKEDDDD');
   $("#funcDesc").html(`Pencil Drawing Tool`);
+  $("#details").html(`Just a little something to help you draw stuff.`);
   currentFunction = new PencilLine(contextReal, lineColor);
 });
 
 $('#graffiti').on('click', function() {
-  $('#funcDesc').html('Graffiti for your punk ass style');
+  $('#funcDesc').html('Graffiti Spray Paint');
+  $("#details").html(`To help you looks like a real punk ass bitch`);
     currentFunction = new Graffiti(contextReal, lineColor);
   });
   $('#marker').on('click', function() {
-    $('#funcDesc').html('Whiteboard Marker');
+    $('#funcDesc').html('Marker');
+    $("#details").html(`for when the pencil just not think enough`);
     currentFunction = new Marker(contextReal, lineColor);
   });
 //------------------Selecter
 $('#selector-button').on('click', function() {
-  $('#funcDesc').html('Selector tool: allows you to cut out the ugly parts of your painting');
+  $('#funcDesc').html('Selector tool');
+  $("#details").html(`Allows you to cut out the ugly parts of your painting`);
   currentFunction = new Selecting(contextReal, contextDraft);
 });
 //------------------Colors
 $('#fill').on('click', function() {
-  $('#funcDesc').html("Paint Fill when you're too lazy to do this manually");
+  $('#funcDesc').html("Paint Fill");
+  $("#details").html(`Because you're too lazy to do this manually`);
   currentFunction = new ColorFill(contextReal, contextDraft);
 });
 
 // David's Clicks
 $('#drawingRect').on('click', function(){
   $('#funcDesc').html('Rectangles baby!');
+  $("#details").html(`That four sided thing`);
   currentFunction = new DrawingRectangle(contextReal,contextDraft)
 })
 
 $('#drawingTriangle').on('click', function(){
   $('#funcDesc').html('Triangles baby!');
+  $("#details").html(`That three sided thing`);
   currentFunction = new DrawingTriangle(contextReal, contextDraft)
 })
 
 $('#drawingCircle').on('click', function(){
   $('#funcDesc').html('Circles Baby!');
+  $("#details").html(`That no sided thing`);
   currentFunction = new DrawingCircle(contextReal, contextDraft)
 })
 
 $('#drawingPolygon').on('click', function(){
   $('#funcDesc').html('PPPOOOLLLYYYGGGOOONNN!!!');
+  $("#details").html(`YEEEEEEEAAAAAA BABYYYYYY`);
   currentFunction = new DrawingPolygon(contextReal, contextDraft)
 });
 
 $('#drawingEraser').on('click', function(){
-  $('#funcDesc').html('Erase that nasty stuff');
+  $('#funcDesc').html('Eraser');
+  $("#details").html(`Only losers make mistakes`);
   currentFunction = new DrawingEraser(contextReal, contextDraft)
 })
 
@@ -55,6 +65,7 @@ $('#drawingEraser').on('click', function(){
 
 $('#drawingImages').on('click', function(){
   $('#funcDesc').html('Pick one of our friendly emojis to add color into your dark lonely existence.');
+  $("#details").html(``);
   $('.imagebox').toggle('show');
 })
 
@@ -139,17 +150,20 @@ $('#imagebox16').click(function(){
 })
   
 $('#clearCanvas').on('click', function(){
-  $('#funcDesc').html('Canvas has been CLEARED');
+  $('#funcDesc').html('Clear Canvas');
+  $("#details").html(`Only failures make big mistakes`);
   new clearCanvas(contextReal, contextDraft)
 })
 
 // Suruchi's Clicks
 $('#straight-line').on('click',function(){
   $('#funcDesc').html('Straight Lines');
+  $("#details").html(``);
   currentFunction = new DrawingStraight(contextReal,contextDraft)
 })
 
 $('#curve-line').on('click',function(){
-  $('#funcDesc').html('Curved Lines: "Liam, I did this with NO help..." - Suruchi, 2019');
+  $('#funcDesc').html('Curved Lines');
+  $("#details").html(`"Liam, I did this with NO help..." - Suruchi, 2019`);
   currentFunction = new DrawingCurve(contextReal,contextDraft)
 })
